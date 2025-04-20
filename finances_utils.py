ebc4107d53620715101d5333e771cff9aaaf0721
Rseiji <uhrafael@gmail.com>
2025-04-20 16:18:32 -0300

    feat: Add utilities for stock transaction processing and database interaction
    - Created `finances_utils.py` to calculate average price, current quantity, and profit for stock transactions.
    - Added `process_new_trades` function to handle new trades incrementally.
    - Introduced `utils.py` for persisting DataFrames to a PostgreSQL database and reading SQL queries.
    - Added a `requirements.txt` file with necessary dependencies for the project.
    - Created some jupyter notebooks for manual processings and general exploration on data before better automating some processes

---
+++ finances_utils.py
@@ -0,0 +1,77 @@
+"""Utilities to help in operational activities."""
+import pandas as pd
+
+
+def calculate_avg_price(df: pd.DataFrame) -> pd.DataFrame:
+    """Given a pd.DataFrame with stocks transactional data, calculate the average price
+    for each row.
+    """
+    for _, group in df.groupby("ticker"):
+        avg_price = 0.0
+        quantity = 0
+
+        for idx, row in group.iterrows():
+            q = row["quantity"]
+            p = row["price"]
+
+            if q > 0:
+                total_cost = avg_price * quantity + p * q
+                quantity += q
+                avg_price = total_cost / quantity if quantity > 0 else 0
+                profit = 0.0
+            else:
+                sale_qty = -q
+                profit = (p - avg_price) * sale_qty
+                quantity -= sale_qty
+
+            df.loc[idx, "avg_price"] = avg_price
+            df.loc[idx, "current_quantity"] = quantity
+            df.loc[idx, "profit"] = profit
+    return df
+
+
+def process_new_trades(
+    df_new: pd.DataFrame,
+    avg_price_start: float | int,
+    quantity_start: float | int,
+) -> pd.DataFrame:
+    """
+    Process new trades incrementally.
+
+    Parameters
+    ----------
+    df_new : pd.DataFrame
+        DataFrame containing new trade events, already sorted by date.
+    avg_price_start : float
+        Current average price before processing new trades.
+    quantity_start : float
+        Current quantity before processing new trades.
+
+    Returns
+    -------
+    pd.DataFrame
+        Updated DataFrame with `avg_price` and `current_quantity` columns.
+    """
+    df_new = df_new.copy()
+    df_new["avg_price"] = 0.0
+    df_new["current_quantity"] = 0
+
+    avg_price: float = avg_price_start
+    quantity: float = quantity_start
+
+    for idx, row in df_new.iterrows():
+        q: float = row["quantity"]
+        p: float = row["price"]
+
+        if q > 0:
+            total_cost: float = avg_price * quantity + p * q
+            quantity += q
+            avg_price = total_cost / quantity if quantity > 0 else 0
+        else:
+            sale_qty: float = -q
+            quantity -= sale_qty
+
+        df_new.loc[idx, "avg_price"] = avg_price
+        df_new.loc[idx, "current_quantity"] = quantity
+
+    return df_new
---
+++ utils.py
@@ -0,0 +1,51 @@
+import pandas as pd
+from sqlalchemy import create_engine
+from datetime import datetime
+import os
+
+PWD = os.getenv("postgres_pwd")
+CONN_STR = f"postgresql+psycopg2://postgres:{PWD}@localhost:5432/finances"
+
+
+def persist_dataframe_to_database(
+    df: pd.DataFrame,
+    schema: str,
+    table: str,
+    assign_processed_at_column: bool = False,
+    conn_str: str = CONN_STR,
+    saving_kwargs: dict | None = None,
+) -> None:
+    if saving_kwargs is None:
+        saving_kwargs = {}
+
+    if assign_processed_at_column:
+        df["_processed_at"] = datetime.now()
+
+    df.to_sql(
+        name=table,
+        schema=schema,
+        con=create_engine(conn_str),
+        if_exists="append",
+        index=False,
+        **saving_kwargs
+    )
+
+
+def read_sql_query(query: str, conn_str: str = CONN_STR) -> pd.DataFrame:
+    """Execute an SQL query and returns the result as a pandas DataFrame.
+
+    Params
+    ------
+    query (str)
+        The SQL query to execute.
+    conn_str (str)
+        The connection string for the PostgreSQL database.
+
+    Returns
+    -------
+    pd.DataFrame
+        The result of the query as a DataFrame.
+    """
+    engine = create_engine(conn_str)
+    with engine.connect() as connection:
+        return pd.read_sql_query(query, connection)
