d5640dcc3c9cdc15d2104565198930002edcb156
Rseiji <uhrafael@gmail.com>
2025-04-21 00:25:07 -0300

    feat: Create initial SQL schema and tables for crypto and stocks data management

---
+++ tables.sql
@@ -0,0 +1,57 @@
+CREATE SCHEMA crypto;
+
+CREATE TABLE crypto.deposits (
+    deposit_date DATE,
+    value_brl FLOAT,
+    exchange_name TEXT,
+    _processed_at TIMESTAMP
+);
+
+CREATE TABLE crypto.usdbrl (
+    date DATE,
+    brl_value FLOAT,
+    _processed_at TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS crypto.swaps (
+    swap_date DATE,
+    received_amount DOUBLE PRECISION NOT NULL,
+    paid_taxes_amount DOUBLE PRECISION NOT NULL,
+    paid_amount DOUBLE PRECISION NOT NULL,
+    received_currency TEXT NOT NULL,
+    paid_taxes_currency TEXT NOT NULL,
+    paid_currency TEXT NOT NULL,
+    exchange_name TEXT,
+    _processed_at TIMESTAMP
+);
+
+CREATE TABLE crypto.earnings (
+    earning_date DATE NOT NULL,
+    earning_currency TEXT NOT NULL,
+    source TEXT NOT NULL,
+    earning_amount DOUBLE PRECISION NOT NULL,
+    _processed_at TIMESTAMP
+);
+
+CREATE TABLE crypto.withdraws (
+    withdraw_date DATE NOT NULL,
+    amount DOUBLE PRECISION NOT NULL,
+    tax DOUBLE PRECISION NOT NULL,
+    currency_amount TEXT NOT NULL,
+    currency_tax TEXT NOT NULL,
+    source TEXT NOT NULL,
+    destiny TEXT NOT NULL
+);
+
+CREATE SCHEMA stocks;
+
+CREATE TABLE stocks.transactions (
+    date DATE NOT NULL,
+    ticker TEXT NOT NULL,
+    quantity DOUBLE PRECISION NOT NULL,
+    price DOUBLE PRECISION NOT NULL,
+    taxes DOUBLE PRECISION NOT NULL,
+    avg_price DOUBLE PRECISION NOT NULL,
+    current_quantity DOUBLE PRECISION NOT NULL,
+    _processed_at TIMESTAMP
+);
